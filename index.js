b015bc48ceb4569f62581905de157eed455aacc1
MounyaDjabbour <132085456+Mounia-djbr@users.noreply.github.com>
2023-09-15 20:14:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static("public"));
