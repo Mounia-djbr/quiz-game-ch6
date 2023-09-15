0f3559cfc73fca1d7a9d08a2d8beb8df8802529f
Mounia-djbr <mouniadjabour@gmail.com>
2023-09-15 19:15:12 +0200

    challenge6

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const PORT = 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const db = fs.readFileSync(path.resolve(__dirname, "db.json"));
+const data = JSON.parse(db);
+
+app.get("/api", (req, res) => {
+  return res.status(200).json({ data: "hello world" });
+});
+
+app.get("/api/level1", (req, res) => {
+  return res.status(200).json({ data: data.level1 });
+});
+
+app.get("/api/level2", (req, res) => {
+  return res.status(200).json({ data: data.level2 });
+});
+
+app.get("/api/level3", (req, res) => {
+  return res.status(200).json({ data: data.level3 });
+});
+
+app.get("/api/level4", (req, res) => {
+  return res.status(200).json({ data: data.level4 });
+});
+
+app.get("/api/level5", (req, res) => {
+  return res.status(200).json({ data: data.level5 });
+});
+
+
+
+app.listen(PORT,() => {
+    console.log(`Server run on port ${PORT}`);
+})
